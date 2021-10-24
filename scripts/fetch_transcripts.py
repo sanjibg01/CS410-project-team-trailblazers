281cb197dbe72b5f73daf81fb3f738f14c561ab9
Paul Zuradzki <paulzuradzki@gmail.com>
2021-10-24 13:11:56 -0500

    altered to convert to one json instead of separate files in one dir

--- scripts/fetch_transcripts.py
+++ scripts/fetch_transcripts.py
@@ -2,7 +2,7 @@
 
 This script gets the transcript files from all the subdirectories returned by coursera-dl.
 
-Coursera files are originall structured in subdirectories like the tree below. It would be easier to work with the data if we stored them in one or two files.
+Coursera files are originall structured in subdirectories like the tree below. It would be easier to work with the data if we stored them in one directory or one merged JSON.
 .
 ├── 01_orientation
 │   ├── 01_about-the-course
@@ -19,7 +19,7 @@ Coursera files are originall structured in subdirectories like the tree below. I
 
 import os
 import re
-import shutil
+import json
 from pathlib import Path
 
 def main():
@@ -30,55 +30,46 @@ def main():
     assert 'text-retrieval' in os.listdir(data_dir), 'missing course files for text-retrieval'
     assert 'text-mining' in os.listdir(data_dir), 'missing course files for text-mining'
 
-    # we need to copy the *.en.srt and *.en.txt English transcript files to one location
-    srt_rgx = re.compile(r'(.*en.srt)')
-    txt_rgx = re.compile(r'(.*en.txt)')
-
     # make coursera_transcripts directory if it does not already exist
     (data_dir / 'coursera_transcripts').mkdir(parents=True, exist_ok=True)
-    (data_dir / 'coursera_transcripts/txt').mkdir(parents=True, exist_ok=True)
-    (data_dir / 'coursera_transcripts/srt').mkdir(parents=True, exist_ok=True)
 
+    # we need to copy the *.en.srt and *.en.txt English transcript files to one location
+    # call converter func 4 times; 2 MOOCs (text retrieval, text mining) * 2 file types (txt, srt)
+    filetype_rgx_lookup = {'txt': r'(.*en.txt)', 'srt': r'(.*en.srt)'}    
+    for course in ['text-retrieval', 'text-mining']:
+        for filetype in filetype_rgx_lookup:
+            convert_transcripts_to_json(source_dir=data_dir / course,
+                                        rgx_pattern=filetype_rgx_lookup[filetype],
+                                        target_filepath=data_dir / f'coursera_transcripts/transcripts_{course}_{filetype}.json')
 
-    # call copy func 4 times; 2 MOOCs (text retrieval, text mining) * 2 file types (txt, srt)
-    copy_txt_files_to_target(source_dir=data_dir / 'text-retrieval',
-                             rgx_pattern=r'(.*en.txt)',
-                             target_dir=data_dir / 'coursera_transcripts/txt')
-
-    copy_txt_files_to_target(source_dir=data_dir / 'text-mining',
-                             rgx_pattern=r'(.*en.txt)',
-                             target_dir=data_dir / 'coursera_transcripts/txt')
-
-    copy_txt_files_to_target(source_dir=data_dir / 'text-retrieval',
-                             rgx_pattern=r'(.*en.srt)',
-                             target_dir=data_dir / 'coursera_transcripts/srt')
-
-    copy_txt_files_to_target(source_dir=data_dir / 'text-mining',
-                             rgx_pattern=r'(.*en.srt)',
-                             target_dir=data_dir / 'coursera_transcripts/srt')
-
-def copy_txt_files_to_target(source_dir, rgx_pattern, target_dir):
-    """Walks a source directory recursively and copies .txt or .srt transcript files to a target directory.
+def convert_transcripts_to_json(source_dir, rgx_pattern, target_filepath):
+    """Walks a source directory recursively and copies .txt or .srt transcript files to a target json / dict-like collection.
 
     Parameters
     ----------
     source_dir: source directory that contains files nested in subdirectories
     rgx_pattern: regular expression pattern to determine which files to copy. Use r'(.*en.srt)' or r'(.*en.txt)' 
-    target_dir: destination directory for copied files
+    target_filepath: destination filepath for consolidate json file
     """
 
     rgx = re.compile(rgx_pattern) # compiled regex    
     src_filepaths = []
 
-    for root, dirs, files in os.walk(data_dir / 'text-retrieval'):
+    for root, dirs, files in os.walk(source_dir):
         for file in files:
             if rgx.search(file) is not None:
                 src_filepaths.append(Path(root) / file)
 
     src_filepaths = sorted(src_filepaths)
     
-    for filepath in srt_filepaths:
-        shutil.copy2(filepath, target_dir / filepath.name)
-    
+    transcripts = {}
+
+    for file in src_filepaths:
+        with open(file, 'r') as f:
+            transcripts[file.name] = f.read()
+
+    with open(target_filepath, 'w') as fp:
+        json.dump(obj=transcripts, fp=fp, indent=1)
+        
 if __name__ == "__main__":
     main()
